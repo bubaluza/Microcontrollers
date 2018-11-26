e5bac002870af21b3752c28ab54c4fbedcfacd34
Gabriel J Schiller <gabrielschiller8@gmail.com>
2018-11-26 16:16:07 -0200

    Update helloWorld.c

--- PIC16F877A/helloWorld/helloWorld.c
+++ PIC16F877A/helloWorld/helloWorld.c
@@ -1,10 +1,28 @@
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#define _XTAL_FREQ 12000000
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
 void main() {
-  TRISD = 0x00;          // set pinD as OUTPUT
+  TRISD = 0x00;                 // set pinD as OUTPUT
 
   do {
-    PORTD = 0x00;        // pinD LOW
-    delay_ms(1000);      // delay 1000ms/1s
-    PORTD = 0xFF;        // pinD HIGH
-    delay_ms(1000);      // delay 1000ms/1s
-  } while(1);            // loop
+    PORTD = 0x00;               // pinD LOW
+    __delay_ms(1000);           // delay 1000ms/1s
+    PORTD = 0xFF;               // pinD HIGH
+    __delay_ms(1000); ;         // delay 1000ms/1s
+  } while(1);                   // loop
 }
+
+
