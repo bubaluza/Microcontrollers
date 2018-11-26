49911cdd6f554d3f433e7af488c9ce743330d6d3
Gabriel J Schiller <gabrielschiller8@gmail.com>
2018-11-25 22:56:19 -0200

    English update

--- 8051/helloWorld/blink.asm
+++ 8051/helloWorld/blink.asm
@@ -1,35 +1,34 @@
-LED	EQU P0
-LED1	EQU P0.0
+LED  	EQU P0				;define LED P0
 
-;INICIAÇÃO
-		MOV SP,#3FH
-		MOV LED,#00h
+;INICIATION
+          MOV SP,#3FH			;stack
+          MOV LED,#00h			;led=0
+;MAIN
+LOOP:     MOV LED,#0FFh			;all P0 pins HIGH
+          LCALL DELAY1S			;call  DELAY1S
+          MOV LED,#00h			;all P0 pins LOW
+          LCALL DELAY1S			;call  DELAY1S
+          LJMP LOOP			;loop call
 
-;PROCESSO
-CICLO:		MOV LED,#0FFh
-		LCALL ATRASO
-		MOV LED,#00h
-		LCALL ATRASO
-		LJMP CICLO
-
-;SUBROTINA DE ATRASO
-
-ATRASO: 	MOV A,#10		;atraso de 1s
-AQUI:		LCALL ATRASO2
-		DJNZ A, AQUI
-		RET
-
-ATRASO2: 	MOV B,#100		;atraso de 100ms
-AQUI2:		LCALL ATRASO3
-		DJNZ B, AQUI2
-		RET
-		
-ATRASO3: 	MOV R0,#150		;atraso de 1ms
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		DJNZ R0, $-5
-		RET
-
+;DELAY FUNCTIONS
+					;1 second delay
+DELAY1S:  MOV A,#10   			;move 10 to accumulator A
+AUX:      LCALL DELAY100		;call DELAY100
+          DJNZ A, AUX			;if A!=0 decrement A and call AUX 
+          RET				;return
+	  
+					;100ms delay
+DELAY100: MOV B,#100    		;move 100 to accumulator B
+AUX2:     LCALL DELAY1MS		;call DELAY1MS
+          DJNZ B, AUX2			;if B!=0 decrement B and call AUX2 
+          RET
+    
+    					;1ms delay
+DELAY1MS: MOV R0,#150   		;move 150 to R0
+          NOP				;NOP to spend machine cycle
+          NOP
+          NOP
+          NOP
+          NOP
+          DJNZ R0, $-5			;if R0!=0 decrement R0 and back 5 lines
+          RET				;return
